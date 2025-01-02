9d30f840914a78f49f6dc2a72a74928fa1ca75d0
linckandrea <linckandrea01@gmail.com>
2025-01-02 02:05:42 +0100

    drivers: ion: sync LA.UM.9.3.r1-02800-sdm845.0 ditching Sony's edits

--- drivers/staging/android/ion/msm/msm_ion.c
+++ drivers/staging/android/ion/msm/msm_ion.c
@@ -637,9 +637,6 @@ static int check_vaddr_bounds(unsigned long start, unsigned long end)
 	struct vm_area_struct *vma;
 	int ret = 1;
 
-	if (!start)
-		goto out;
-
 	if (end < start)
 		goto out;
 
@@ -837,28 +834,20 @@ long msm_ion_custom_ioctl(struct ion_client *client,
 
 		down_read(&mm->mmap_sem);
 
-		if ((unsigned long)data.flush_data.vaddr >
-				(ULONG_MAX - data.flush_data.offset)) {
-			pr_err("%s: Integer overflow detected for %pK\n",
+		start = (unsigned long)data.flush_data.vaddr +
+			data.flush_data.offset;
+		end = start + data.flush_data.length;
+
+		if (start && check_vaddr_bounds(start, end)) {
+			pr_err("%s: virtual address %pK is out of bounds\n",
 			       __func__, data.flush_data.vaddr);
 			ret = -EINVAL;
 		} else {
-			start = (unsigned long)data.flush_data.vaddr +
-				data.flush_data.offset;
-			end = start + data.flush_data.length;
-
-			if (check_vaddr_bounds(start, end)) {
-				pr_err("%s: virtual address %pK is out of bounds\n",
-				       __func__, data.flush_data.vaddr);
-				ret = -EINVAL;
-			} else {
-				ret = ion_do_cache_op(
-					client, handle, data.flush_data.vaddr,
-					data.flush_data.offset,
-					data.flush_data.length, cmd);
-			}
+			ret = ion_do_cache_op(
+				client, handle, data.flush_data.vaddr,
+				data.flush_data.offset,
+				data.flush_data.length, cmd);
 		}
-
 		up_read(&mm->mmap_sem);
 
 		ion_free_nolock(client, handle);
@@ -972,8 +961,17 @@ int msm_ion_heap_alloc_pages_mem(struct pages_mem *pages_mem)
 	pages_mem->free_fn = kfree;
 	page_tbl_size = sizeof(struct page *) * (pages_mem->size >> PAGE_SHIFT);
 	if (page_tbl_size > SZ_8K) {
-		pages = vmalloc(page_tbl_size);
-		pages_mem->free_fn = vfree;
+		/*
+		 * Do fallback to ensure we have a balance between
+		 * performance and availability.
+		 */
+		pages = kmalloc(page_tbl_size,
+				__GFP_COMP | __GFP_NORETRY |
+				__GFP_NOWARN);
+		if (!pages) {
+			pages = vmalloc(page_tbl_size);
+			pages_mem->free_fn = vfree;
+		}
 	} else {
 		pages = kmalloc(page_tbl_size, GFP_KERNEL);
 	}
